7b4c7e940fc6a475733d48c07f31d305537909f1
Bridjet Mendyuk <bjmendy@Bridjets-MacBook-Pro.local>
2017-06-27 12:24:40 -0500

    first react commit

---
+++ clientReact/main.js
@@ -0,0 +1,18 @@
+var React    = require('react');
+var ReactDOM = require('react-dom');
+var request  = require('superagent');
+
+//creating our first component
+
+
+
+
+
+
+
+
+//last component
+
+ReactDOM.render(
+  <MainContainer/>, document.getElementById('container')
+  )
